4aa4de450f3fee102ea963917cfaaf6b09a5eaef
Amit <apk.karande@gmail.com>
2013-11-13 00:37:35 -0800

    Added lines to make the initial folder and create alias within the script itself when it is invoked for the first time

--- cat-diary.sh
+++ cat-diary.sh
@@ -1,7 +1,21 @@
+###################################################################
+#	            CAT-DIARY BY AMIT KARANDE
+###################################################################
+
+echo -e "\n*******CAT-DIARY (CATD)*******"
+CURR_DIR=`pwd`
+DIARY_DIR=~/Diary/
+if [ ! -d "$DIARY_DIR" ]
+then
+echo -e "\nSeems like a new user...Creating Diary folder...Adding alias.. \n\nHope you enjoy using me.. =)\n\nYou can call me 'catd'\n\n"
+echo "alias catd=\"bash $CURR_DIR/cat-diary.sh\"" >> ~/.bashrc
+source ~/.bashrc
+mkdir $DIARY_DIR
+fi
 TODAY=$(date)
 YEAR=$( date | awk '{print $NF}')
-DIARY_FILE="~/Diary/$YEAR.txt"
-if [ ! -e $DIARY_FILE ]
+DIARY_FILE=~/Diary/$YEAR.txt
+if [ ! -e "$DIARY_FILE" ]
 then
 echo "Creating new diary for the year $YEAR.....Happy writing!! =)"
 touch $DIARY_FILE
@@ -10,6 +24,6 @@ touch $DIARY_FILE
 fi 
 echo $TODAY >> $DIARY_FILE
 echo "============================" >> $DIARY_FILE
-echo -e "Press Ctrl-D after writing...\n"
+echo -e "Press Enter followed by Ctrl+D after writing...\n"
 cat >> $DIARY_FILE
 echo -e "\n" >> $DIARY_FILE
